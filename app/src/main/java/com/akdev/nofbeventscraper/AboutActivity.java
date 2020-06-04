bbadfef7d35a8ff6663ad0f459afa7b520a23a3d
akaessens <>
2020-06-04 18:10:41 +0200

    generate icons and banners add donation button

--- app/src/main/java/com/akdev/nofbeventscraper/AboutActivity.java
+++ app/src/main/java/com/akdev/nofbeventscraper/AboutActivity.java
@@ -2,7 +2,11 @@ package com.akdev.nofbeventscraper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+        ImageView img = (ImageView)findViewById(R.id.paypal_image);
+
+        img.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.paypal.me/andreaskaessens"));
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
